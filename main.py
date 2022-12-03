69e8508690aab20638e9845db137a8dc5d1a9012
Wanice <52546308+wanicedude@users.noreply.github.com>
2022-12-03 15:02:33 +0100

    update

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pandas
 
 alpha_data = pandas.read_csv(
     "/Users/wanice/Documents/GitHub/NATO-alphabet/nato_phonetic_alphabet.csv")
+
+
+alpha_dict = {row.letter: row.code for (index, row) in alpha_data.iterrows()}
+
+
+print(alpha_dict)
+print(alpha_dict.keys())
+
+guess = input("Kindly input a letter: ").upper()
+newt = []
+for char in guess:
+    if char in alpha_dict.keys():
+        newt.append(alpha_dict.get(char))
+print(newt)
